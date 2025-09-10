ec68fe5830e36a7ece63ab326350ae3a87dec209
hieutrinh1610 <fanmarvel1610@gmail.com>
2025-09-11 00:07:10 +0700

    add_eatingroom

---
+++ VR Room/Assets/_Course Library/PhongAn_Hieu/CodePhongAn_hieu/BowlRoomManager.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[DisallowMultipleComponent]
+public class BowlRoomManager : MonoBehaviour
+{
+    public List<KitchenSocketValidator> sockets;
+    public UnityEvent OnWrongPlacement;
+    public UnityEvent<int, int> OnProgressChanged;
+    public UnityEvent OnAllCompleted;
+
+    int correctCount;
+
+    void OnEnable()
+    {
+        foreach (var s in sockets)
+        {
+            s.OnPlacedCorrect.AddListener(HandleCorrect);
+            s.OnPlacedWrong.AddListener(HandleWrong);
+            s.OnItemRemoved.AddListener(HandleRemoved);
+        }
+        Recount();
+    }
+
+    void OnDisable()
+    {
+        foreach (var s in sockets)
+        {
+            s.OnPlacedCorrect.RemoveListener(HandleCorrect);
+            s.OnPlacedWrong.RemoveListener(HandleWrong);
+            s.OnItemRemoved.RemoveListener(HandleRemoved);
+        }
+    }
+
+    void HandleCorrect(KitchenSocketValidator s)
+    {
+        Recount();
+        if (correctCount >= sockets.Count)
+            OnAllCompleted?.Invoke();
+    }
+
+    void HandleWrong(KitchenSocketValidator s)
+    {
+        OnWrongPlacement?.Invoke();
+    }
+
+    void HandleRemoved(KitchenSocketValidator s)
+    {
+        Recount();
+    }
+
+    void Recount()
+    {
+        int c = 0;
+        foreach (var s in sockets) if (s.IsCorrectlyOccupied()) c++;
+        correctCount = c;
+        OnProgressChanged?.Invoke(correctCount, sockets.Count);
+    }
+}
