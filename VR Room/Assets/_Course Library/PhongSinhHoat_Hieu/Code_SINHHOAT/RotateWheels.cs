b7e7068cdf03cc9f0e1d8b3dbbda4cda3b0f12f0
hieutrinh1610 <fanmarvel1610@gmail.com>
2025-09-11 16:40:36 +0700

    add_maychieu

---
+++ VR Room/Assets/_Course Library/PhongSinhHoat_Hieu/Code_SINHHOAT/RotateWheels.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class WheelRotator : MonoBehaviour
+{
+    [Header("Spin Settings")]
+    [SerializeField] float spinSpeed = 360f;   // độ/giây
+    [SerializeField] float spinDuration = 2f;  // thời gian quay
+
+    bool spinning;
+
+    public void RotateWheel()
+    {
+        if (!spinning)
+            StartCoroutine(SpinCoroutine());
+    }
+
+    System.Collections.IEnumerator SpinCoroutine()
+    {
+        spinning = true;
+        float t = 0f;
+        while (t < spinDuration)
+        {
+            float dt = Time.deltaTime;
+            transform.Rotate(0f, 0f, spinSpeed * dt, Space.Self);
+            t += dt;
+            yield return null;
+        }
+        spinning = false;
+    }
+}
